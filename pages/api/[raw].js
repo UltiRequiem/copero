ee2532c314fbb84bf555cbaeda19d00e3453ac9b
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-11-02 09:51:25 -0500

    :recycle: Update

--- pages/api/[raw].js
+++ pages/api/[raw].js
@@ -12,5 +12,5 @@ export default async function handleRaw({ query: { raw }, method }, response) {
   }
 
   response.statusCode = 200;
-  response.send(snippetObject.snippet);
+  response.send(snippetObject?.snippet);
 }
