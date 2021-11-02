a3b6eb217f6f7586811a51ee6bc3bf04d63ce489
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-11-02 14:21:24 -0500

    :sparkles: Add List Page

--- pages/list.jsx
+++ pages/list.jsx
@@ -1,3 +1,10 @@
 import React from 'react';
 
-export default function ListSnippets(properties) {}
+// eslint-disable-next-line no-unused-vars
+export default function ListSnippets(properties) {
+  return (
+    <>
+      <h1>Todo</h1>
+    </>
+  );
+}
