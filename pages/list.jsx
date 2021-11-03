4bb1c8b1665537beee9ed59b7f2e0b4031ac1cc4
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-11-02 19:17:26 -0500

    :sparkles: List Endpoint

--- pages/list.jsx
+++ pages/list.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
 
-// eslint-disable-next-line no-unused-vars
-export default function ListSnippets(properties) {
+export default function ListSnippets({ snippetsData, host }) {
   return (
     <>
-      <h1>Todo</h1>
+      <h1>All snippets</h1>
+
+      {snippetsData.map((snippet) => (
+        <div>
+          <a href={`http://${host}/${snippet.slug}`}>
+            Visit
+            {' '}
+            {snippet.slug}
+          </a>
+        </div>
+      ))}
     </>
   );
+}
+
+// eslint-disable-next-line unicorn/prevent-abbreviations
+export async function getServerSideProps({
+  req: {
+    headers: { host },
+  },
+}) {
+  const response = await fetch(`http://${host}/api/list`);
+  const parsedResponse = await response.json();
+
+  const snippetsData = parsedResponse.map((item) => ({
+    slug: item.slug,
+  }));
+
+  return {
+    props: {
+      snippetsData,
+      host,
+    },
+  };
 }
