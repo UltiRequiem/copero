dec0dde7ceb9b657ce41f4d2e6a554df7456b17b
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-11-04 22:09:35 +0000

    :recycle: Better Head

--- src/componets/head.jsx
+++ src/componets/head.jsx
@@ -5,6 +5,8 @@ const CustomHead = () => (
   <Head>
     <title>Copero</title>
     <meta charSet="UTF-8" />
+    <meta name="author" content="Eliaz Bobadilla" />
+    <meta name="description" content="Store plain text." />
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     <link rel="icon" type="image/svg" href="/favicon.svg" />
   </Head>
