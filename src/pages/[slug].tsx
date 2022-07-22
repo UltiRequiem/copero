6d558b7a95a55caabe9350ef8d1e432ed89d828c
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-07-22 16:46:22 -0500

    :recycle: Update

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -8,35 +8,34 @@ interface Props {
 }
 
 const CreateSnippet: NextPage<Props> = ({ snippetText, slug }) => {
-  const router = useRouter();
-
   return (
     <div className="text-center">
+      <h1 className="text-xl underline m-2">{slug}</h1>
       <div className="p-3">
-        <button
-          className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded m-2"
-          type="button"
-          onClick={() => copy(window.location.toString())}
-        >
-          Copy Link to Clipboard
-        </button>
-
         <button
           className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
           type="button"
-          onClick={() => router.push(`/api/${slug}`)}
+          onClick={() => copy(snippetText)}
         >
-          Raw Text
+          Copy Content
         </button>
       </div>
 
       <div>
         <textarea
-          className="m-3 p-2 w-72 md:w-96 h-52 m:h-72 lg:h-96 lg:w-96"
+          className="m-3 p-2 w-72 h-72"
           readOnly
           defaultValue={snippetText}
         />
       </div>
+
+      <button
+        className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded m-2"
+        type="button"
+        onClick={() => copy(window.location.toString())}
+      >
+        Copy Shareable Link
+      </button>
     </div>
   );
 };
